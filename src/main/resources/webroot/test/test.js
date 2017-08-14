ac6bd7daaca08b4e62b180b8539783afc386fb51
chhch <chhch+github@protonmail.com>
2017-08-14 15:37:24 +0200

    Refactor test sites

--- src/main/resources/webroot/test/test.js
+++ src/main/resources/webroot/test/test.js
@@ -1,112 +1,123 @@
 $(document).ready(function () {
     var eventBus = new vertx.EventBus('/eventbus/');
 
+    var $lock = $('#lock');
+    var $output = $('#output');
+    var $eventBusAddress = $('#eventBusAddress');
+    var eventBusAddressValue = $eventBusAddress.val();
+    var $eventBusAddressAppend = $('#eventBusAddressAppend');
+    var $json = $('#json');
+    var $sendJson = $('#sendJson');
+    var $registerHandler = $('#registerHandler');
+
     eventBus.onopen = function () {
-        $('i.fa-lock').addClass('fa-unlock');
-        $('i.fa-lock').removeClass('fa-lock');
+        $lock.addClass('fa-unlock');
+        $lock.removeClass('fa-lock');
     };
     eventBus.onclose = function () {
-        $('i.fa-unlock').addClass('fa-lock');
-        $('i.fa-unlock').removeClass('fa-unlock');
+        $lock.addClass('fa-lock');
+        $lock.removeClass('fa-unlock');
     };
 
-    $('#eventBusAddress').change(function () {
-        $('#eventBusAdressAppend').prop("disabled", true);
-        setSendJsonAsPrimaryButton();
-
+    $eventBusAddress.change(function () {
         switch ($(this).val()) {
             case 'chat.add.contact':
-                $('#json').val('{ "contact" : "<name>" }');
+                $json.val('{ "contact" : "<name>" }');
+                $eventBusAddressAppend.prop("disabled", true);
+                setSendJsonAsPrimaryButton();
                 break;
             case 'chat.receive.contact':
-                $('#json').val('<name>');
-                $('#eventBusAdressAppend').prop("disabled", false);
+                $json.val('<name>');
+                $eventBusAddressAppend.prop("disabled", false);
                 setRegisterHandlerAsPrimaryButton();
                 break;
             case 'chat.get.contactList':
-                $('#json').val('null');
+                $json.val('null');
+                $eventBusAddressAppend.prop("disabled", true);
                 setNonePrimaryButton();
                 break;
             case 'chat.send.message':
-                $('#json').val('{ "message" : "<message>", "receiver" : "<name>" }');
+                $json.val('{ "message" : "<message>", "receiver" : "<name>" }');
+                $eventBusAddressAppend.prop("disabled", true);
+                setSendJsonAsPrimaryButton();
                 break;
             case 'chat.receive.message':
-                $('#json').val('{ "date" : <2011-12-03T10:15:30+01:00>, "sender" : "<name>", "receiver" : "<name>", "message" : "<messsage>", "read" : <true/false> }');
-                $('#eventBusAdressAppend').prop("disabled", false);
+                $json.val('{ "date" : <2011-12-03T10:15:30+01:00>, "sender" : "<name>", "receiver" : "<name>", "message" : "<messsage>", "read" : <true/false> }');
+                $eventBusAddressAppend.prop("disabled", false);
                 setRegisterHandlerAsPrimaryButton();
                 break;
             case 'chat.send.read.notification':
-                $('#json').val('{ "sender" : "<name>" }');
+                $json.val('{ "sender" : "<name>" }');
+                $eventBusAddressAppend.prop("disabled", true);
+                setSendJsonAsPrimaryButton();
                 break;
             case 'chat.receive.read.notification':
-                $('#json').val('{ "receiver" : "<name>", "count" : 1 }');
-                $('#eventBusAdressAppend').prop("disabled", false);
+                $json.val('{ "receiver" : "<name>", "count" : 1 }');
+                $eventBusAddressAppend.prop("disabled", false);
                 setRegisterHandlerAsPrimaryButton();
                 break;
             case 'chat.get.conversation':
-                $('#json').val('{ "contact" : "<name>" }');
+                $json.val('{ "contact" : "<name>", "onlyUnread": false }');
+                $eventBusAddressAppend.prop("disabled", true);
+                setSendJsonAsPrimaryButton();
                 break;
             default:
-                $('#json').val('');
+                $json.val('');
         }
     });
 
-    $('#sendJson').click(function () {
-        var eventBusAddresse = $('#eventBusAddress').val();
-        if ($('#eventBusAdressAppend').prop("disabled") === false && $('#eventBusAdressAppend').val().trim().length > 0) {
-            eventBusAddresse += "." + $("#eventBusAdressAppend").val();
+    $sendJson.click(function () {
+        if ($eventBusAddressAppend.prop("disabled") === false && $eventBusAddressAppend.val().trim().length > 0) {
+            eventBusAddressValue += "." + $eventBusAddressAppend.val();
         }
 
         var json = $.parseJSON($('#json').val());
-        print("Send to '" + eventBusAddresse + "':\n" + JSON.stringify(json, null, 2));
-        eventBus.send(eventBusAddresse, json, replyHandler);
+        print("Send to '" + eventBusAddressValue + "':\n" + JSON.stringify(json, null, 2));
+        eventBus.send(eventBusAddressValue, json, replyHandler);
     });
 
-    $('#registerHandler').click(function () {
-        var eventBusAddresse = $('#eventBusAddress').val();
-        if ($('#eventBusAdressAppend').prop("disabled") === false && $('#eventBusAdressAppend').val().trim().length > 0) {
-            eventBusAddresse += "." + $("#eventBusAdressAppend").val();
+    $registerHandler.click(function () {
+        if ($eventBusAddressAppend.prop("disabled") === false && $eventBusAddressAppend.val().trim().length > 0) {
+            eventBusAddressValue += "." + $eventBusAddressAppend.val();
         }
 
-
-        print("Register to '" + eventBusAddresse + "'.");
-        eventBus.registerHandler(eventBusAddresse, function (message) {
-            print("Received from '" + eventBusAddresse + "':\n" + JSON.stringify(message, null, 2));
+        print("Register to '" + eventBusAddressValue + "'.");
+        eventBus.registerHandler(eventBusAddressValue, function (message) {
+            print("Received from '" + eventBusAddressValue + "':\n" + JSON.stringify(message, null, 2));
         });
     });
+
+    var replyHandler = function (reply, reply_err) {
+        print("Replied:\n" + JSON.stringify(reply, null, 2));
+        print("Replied:\n" + JSON.stringify(reply_err, null, 2));
+    };
+
+    var print = function (message) {
+        if ($output.val().length === 0) {
+            $output.val(message + "\n");
+        } else {
+            $output.val($output.val() + "---\n" + message + "\n");
+        }
+    };
+
+    var setSendJsonAsPrimaryButton = function () {
+        $sendJson.addClass('btn-primary');
+        $sendJson.removeClass('btn-default');
+        $registerHandler.addClass('btn-default');
+        $registerHandler.removeClass('btn-primary');
+    };
+
+    var setRegisterHandlerAsPrimaryButton = function () {
+        $sendJson.addClass('btn-default');
+        $sendJson.removeClass('btn-primary');
+        $registerHandler.addClass('btn-primary');
+        $registerHandler.removeClass('btn-default');
+    };
+
+    var setNonePrimaryButton = function () {
+        $sendJson.addClass('btn-default');
+        $sendJson.removeClass('btn-primary');
+        $registerHandler.addClass('btn-default');
+        $registerHandler.removeClass('btn-primary');
+    };
 });
-
-var replyHandler = function (reply, reply_err) {
-    print("Replied:\n" + JSON.stringify(reply, null, 2));
-//  print("Replied:\n" + JSON.stringify(reply_err, null, 2));
-};
-
-var print = function (message) {
-    if ($('#output').val().length === 0) {
-        $('#output').val(message + "\n");
-    } else {
-        $('#output').val($('#output').val() + "---\n" + message + "\n");
-    }
-};
-
-var setSendJsonAsPrimaryButton = function () {
-    $('#sendJson').addClass('btn-primary');
-    $('#sendJson').removeClass('btn-default');
-    $('#registerHandler').addClass('btn-default');
-    $('#registerHandler').removeClass('btn-primary');
-};
-
-var setRegisterHandlerAsPrimaryButton = function () {
-    $('#sendJson').addClass('btn-default');
-    $('#sendJson').removeClass('btn-primary');
-    $('#registerHandler').addClass('btn-primary');
-    $('#registerHandler').removeClass('btn-default');
-};
-
-var setNonePrimaryButton = function () {
-    $('#sendJson').addClass('btn-default');
-    $('#sendJson').removeClass('btn-primary');
-    $('#registerHandler').addClass('btn-default');
-    $('#registerHandler').removeClass('btn-primary');
-};
-
